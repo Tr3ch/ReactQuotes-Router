460139bc4bd940d08db442b4a71dbe3f462b0b3e
Tr3ch <polak.martin16@gmail.com>
2021-12-12 16:22:06 +0100

    Initial Commit

---
+++ src/pages/AllQuotes.js
@@ -0,0 +1,44 @@
+import { useEffect } from 'react';
+import useHttp from '../hooks/use-http';
+import QuoteList from '../components/quotes/QuoteList';
+import NoQuotesFound from '../components/quotes/NoQuotesFound';
+import { getAllQuotes } from '../lib/api';
+import LoadingSpinner from '../components/UI/LoadingSpinner';
+
+// const DUMMY_QUOTES = [
+//   { id: 'q1', author: 'Max', text: 'Learning React is fun!' },
+//   { id: 'q2', author: 'Maxalim', text: 'Learning React is Great!' },
+// ];
+
+const AllQuotes = () => {
+  const {
+    sendRequest,
+    status,
+    data: loadedQuotes,
+    error,
+  } = useHttp(getAllQuotes, true);
+
+  useEffect(() => {
+    sendRequest();
+  }, [sendRequest]);
+
+  if (status === 'pending') {
+    return (
+      <div className='centered'>
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <p className='centered focused'>{error}</p>;
+  }
+
+  if (status === 'completed' && (!loadedQuotes || loadedQuotes.length === 0)) {
+    return <NoQuotesFound />;
+  }
+
+  return <QuoteList quotes={loadedQuotes} />;
+};
+
+export default AllQuotes;
